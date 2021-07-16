c248cd14cd520c8561fe0f5d2349fff7d6fbafcd
Sydney Bear <sbear3915@gmail.com>
2021-07-16 17:36:36 -0600

    Fix rating and genres

--- src/components/ApiCalls.js
+++ src/components/ApiCalls.js
@@ -4,7 +4,7 @@ export const findMovie = (id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     .then(response => response.json())
     // .then(data => console.log(data.movie.genres, 'data inside of api calls'))
-    // .then(data => formatMovieDetails(data.movie.genres))
+    .then(data => formatMovieDetails(data))
 }
 
 export const allMovies = () => {
--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,4 +1,8 @@
-// export const formatMovieDetails = (data) => {
-//   console.log(data, "THIS IS DATA")
-//   return data.genres.join(' ')
-// }
+export const formatMovieDetails = (data) => {
+  console.log(data, "THIS IS DATA")
+  data.movie.genres = data.movie.genres.join(' ')
+  data.movie.average_rating = data.movie.average_rating.toFixed(2)
+  console.log(data.movie.genres, "this is the data after join")
+  return data
+}
+
