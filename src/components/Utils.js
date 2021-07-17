4378b3f6ca49764e4fe18372c7227802b0e741e7
Sydney Bear <sbear3915@gmail.com>
2021-07-17 14:23:10 -0600

    Update release date to have just the year

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -2,6 +2,7 @@ export const formatMovieDetails = (data) => {
   console.log(data, "THIS IS DATA")
   data.movie.genres = data.movie.genres.join(', ')
   data.movie.average_rating = data.movie.average_rating.toFixed(2)
+  data.movie.release_date = data.movie.release_date.split('-').shift();
   console.log(data.movie.genres, "this is the data after join")
   return data
 }
